f53622d2c1db9778b1abc05ab3b1cc95f735b19d
Barış Zeren <baris@leodrive.ai>
2024-10-17 12:44:35 +0300

    chore: close yolo verbose
    Signed-off-by: Barış Zeren <baris@leodrive.ai>

--- autoware_rosbag2_anonymizer/model/yolo.py
+++ autoware_rosbag2_anonymizer/model/yolo.py
@@ -1,3 +1,6 @@
+import os
+os.environ['YOLO_VERBOSE'] = 'False'
+
 from ultralytics import YOLO
 
 import supervision as sv
@@ -5,7 +8,7 @@ import supervision as sv
 
 class Yolo:
     def __init__(self, model_path: str):
-        self.model = YOLO(model_path)
+        self.model = YOLO(model_path, verbose=False)
 
     def __call__(self, image, confidence) -> sv.Detections:
         results = self.model.predict(image, conf=confidence)
