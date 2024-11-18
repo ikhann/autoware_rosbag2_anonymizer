97cec9f869be328008027e4c992929b0c5c52fba
Khaninaev, Daniil <daniil.khaninaev@intel.com>
2024-11-18 19:49:24 +0200

    Added changes for hackathon
    Rosbag changed to live topic | Added azimuth and camera info data | Added  parameter set for promts

--- autoware_rosbag2_anonymizer/tools/anonymize_with_unified_model.py
+++ autoware_rosbag2_anonymizer/tools/anonymize_with_unified_model.py
@@ -1,118 +1,207 @@
 import os
-
-import cv_bridge
-
-import supervision as sv
 import cv2
+import rclpy
+from rcl_interfaces.msg import SetParametersResult
+from rclpy.node import Node
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge
+import math
+import supervision as sv
 
-from autoware_rosbag2_anonymizer.rosbag_io.rosbag_reader import RosbagReader
-from autoware_rosbag2_anonymizer.rosbag_io.rosbag_writer import RosbagWriter
-
-from autoware_rosbag2_anonymizer.model.unified_language_model import (
-    UnifiedLanguageModel,
-)
+from autoware_rosbag2_anonymizer.model.unified_language_model import UnifiedLanguageModel
 from autoware_rosbag2_anonymizer.model.sam2 import SAM2
-
-from autoware_rosbag2_anonymizer.common import (
-    create_classes,
-    blur_detections,
-    get_file_paths,
-)
+from autoware_rosbag2_anonymizer.common import create_classes
 
 
-def anonymize_with_unified_model(config_data, json_data, device) -> None:
-    # Segment-Anything-2 parameters
-    SAM2_MODEL_CFG = config_data["segment_anything_2"]["model_cfg"]
-    SAM_CHECKPOINT_PATH = config_data["segment_anything_2"]["checkpoint_path"]
+class ImageAnonymizerNode(Node):
+    def __init__(self, config_data, json_data, device):
+        super().__init__("image_anonymizer_node")
+        self.bridge = CvBridge()
+        self.config_data = config_data
+        self.json_data = json_data
+        self.device = device
 
-    # Segment-Anything-2
-    sam2 = SAM2(SAM2_MODEL_CFG, SAM_CHECKPOINT_PATH, device)
+        # Initialize models
+        self.sam2 = SAM2(
+            config_data["segment_anything_2"]["model_cfg"],
+            config_data["segment_anything_2"]["checkpoint_path"],
+            device,
+        )
+        self.unified_language_model = UnifiedLanguageModel(config_data, json_data, device)
 
-    unified_language_model = UnifiedLanguageModel(config_data, json_data, device)
-
-    # Get rosbag paths from input folder
-    rosbag2_paths = get_file_paths(
-        config_data["rosbag"]["input_bags_folder"], [".db3", ".mcap"]
-    )
-
-    # Create output folder if it is not exist
-    os.makedirs(config_data["rosbag"]["output_bags_folder"], exist_ok=True)
-
-    for rosbag2_path in rosbag2_paths:
-        reader = RosbagReader(rosbag2_path, 1)
-        writer = RosbagWriter(
-            os.path.join(
-                config_data["rosbag"]["output_bags_folder"],
-                rosbag2_path.split("/")[-1].split(".")[0],
-            ),
-            config_data["rosbag"]["output_save_compressed_image"],
-            config_data["rosbag"]["output_storage_id"],
-            reader.get_qos_profile_map(),
+        # Subscription to the input image topic
+        self.subscription = self.create_subscription(
+            Image,
+            config_data["ros"]["input_image_topic"],
+            self.image_callback,
+            1
         )
 
-        for i, (msg, is_image) in enumerate(reader):
-            if not is_image:
-                writer.write_any(msg.data, msg.type, msg.topic, msg.timestamp)
-            else:
-                # Convert image msg to cv.Mat
-                if "Compressed" in msg.type:
-                    image = cv_bridge.CvBridge().compressed_imgmsg_to_cv2(msg.data)
-                else:
-                    image = cv_bridge.CvBridge().imgmsg_to_cv2(msg.data)
+        # Subscription to the input camera info topic
+        self.camera_info_sub = self.create_subscription(
+            CameraInfo,
+            config_data["ros"]["input_camera_info_topic"],
+            self.camera_info_callback,
+            1
+        )
 
-                # Find bounding boxes with Unified Model
-                detections = unified_language_model(image)
+        self.declare_parameter("prompts", ["face", "box"])
+        self.add_on_set_parameters_callback(self.on_prompts_update)
 
-                # Run SAM2
-                detections = sam2(image=image, detections=detections)
+        # Camera intrinsic parameters
+        self.fx = None
+        self.fy = None
+        self.cx = None
+        self.cy = None
 
-                # Blur detections
-                output = blur_detections(
-                    image,
-                    detections,
-                    config_data["blur"]["kernel_size"],
-                    config_data["blur"]["sigma_x"],
+        # Create classes for annotations
+        self.DETECTION_CLASSES, self.CLASSES, self.CLASS_MAP = create_classes(json_data)
+
+
+    def on_prompts_update(self, params):
+        for param in params:
+            if param.name == "prompts" and isinstance(param.value, list):
+                new_prompts = param.value
+                self.get_logger().info(f"Updating prompts: {new_prompts}")
+
+                # Ensure json_data contains all prompts with empty fields for 'should_inside' and 'should_not_inside'
+                self.json_data["prompts"] = [
+                    {"prompt": prompt, "should_inside": [], "should_not_inside": []}
+                    for prompt in new_prompts
+                ]
+
+                # Update the UnifiedLanguageModel with the new json_data
+                self.unified_language_model = UnifiedLanguageModel(
+                    self.config_data, self.json_data, self.device
                 )
 
-                # Write blured image to rosbag
-                writer.write_image(output, msg.topic, msg.timestamp)
+                self.DETECTION_CLASSES, self.CLASSES, self.CLASS_MAP = create_classes(self.json_data)
 
-                # Print detections: how many objects are detected in each class
-                if config_data["debug"]["print_on_terminal"]:
-                    print("\nDetections:")
-                    for class_id in range(
-                        len(unified_language_model.detection_classes)
-                    ):
-                        print(
-                            f"{unified_language_model.detection_classes[class_id]}: {len([d for d in detections if d[3] == class_id])}"
-                        )
+                self.get_logger().info(
+                    f"Prompts and models updated successfully: {self.json_data['prompts']}")
 
-                # Show debug image
-                if config_data["debug"]["show_on_image"]:
-                    DETECTION_CLASSES, CLASSES, CLASS_MAP = create_classes(
-                        json_data=json_data
-                    )
+        return SetParametersResult(successful=True)
 
-                    bounding_box_annotator = sv.BoxAnnotator()
-                    annotated_image = bounding_box_annotator.annotate(
-                        scene=output,
-                        detections=detections,
-                    )
 
-                    labels = [
-                        f"{DETECTION_CLASSES[class_id]} {confidence:0.2f}"
-                        for _, _, confidence, class_id, _, _ in detections
-                    ]
-                    label_annotator = sv.LabelAnnotator()
-                    annotated_image = label_annotator.annotate(
-                        output,
-                        detections,
-                        labels,
-                    )
+    def image_callback(self, msg):
+        if self.fx is None:
+            return
 
-                    height, width = image.shape[:2]
-                    annotated_image = cv2.resize(
-                        annotated_image, (width // 2, height // 2)
-                    )
-                    cv2.imshow("anonymizer debug", annotated_image)
-                    cv2.waitKey(1)
+        self.get_logger().info("Received an image message")
+        # Convert ROS Image message to OpenCV image
+        image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+        # Perform anonymization
+        anonymized_image, detections = self.anonymize_image(image)
+
+        # Log detections
+        for i, (bbox, confidence, class_id) in enumerate(
+                zip(detections.xyxy, detections.confidence, detections.class_id)
+        ):
+            x_min, y_min, x_max, y_max = bbox
+            width = x_max - x_min
+            height = y_max - y_min
+
+            self.get_logger().info(
+                f"Detection {i}: Class={self.DETECTION_CLASSES[class_id]}, "
+                f"Confidence={confidence:.2f}, Width={width}, Height={height}"
+            )
+
+        # Print debug information
+        if self.config_data["debug"]["print_on_terminal"]:
+            self.print_detections(detections)
+
+        # Display the processed image
+        if self.config_data["debug"]["show_on_image"]:
+            self.display_debug_image(anonymized_image, detections)
+
+        # Compute azimuth for each detection
+        for i, (bbox, confidence, class_id) in enumerate(
+                zip(detections.xyxy, detections.confidence, detections.class_id)
+        ):
+            x_min, y_min, x_max, y_max = bbox
+            x_center = (x_min + x_max) / 2
+            y_center = (y_min + y_max) / 2
+
+            # Convert to normalized camera coordinates
+            x_norm = (x_center - self.cx) / self.fx
+            y_norm = (y_center - self.cy) / self.fy
+
+            # Calculate azimuth in degrees
+            azimuth_radians = math.atan(x_norm)
+            azimuth_degrees = math.degrees(azimuth_radians)
+
+            self.get_logger().info(
+                f"Detection {i}: Class={self.DETECTION_CLASSES[class_id]}, "
+                f"Confidence={confidence:.2f}, Azimuth={azimuth_degrees:.2f} degrees"
+            )
+
+
+    def camera_info_callback(self, msg):
+        self.get_logger().info("Received an image info message")
+        self.fx = msg.k[0]
+        self.fy = msg.k[4]
+        self.cx = msg.k[2]
+        self.cy = msg.k[5]
+        self.get_logger().info(f"Camera intrinsics set: fx={self.fx}, fy={self.fy}, cx={self.cx}, cy={self.cy}")
+        camera_info_sub = None
+        self.destroy_subscription(self.camera_info_sub)
+
+    def anonymize_image(self, image):
+        # Detect objects with the Unified Model
+        detections = self.unified_language_model(image)
+
+        # Refine detections using SAM2
+        detections = self.sam2(image=image, detections=detections)
+
+        # Blur detections
+        # output = blur_detections(
+        #     image,
+        #     detections,
+        #     self.config_data["blur"]["kernel_size"],
+        #     self.config_data["blur"]["sigma_x"],
+        # )
+        return image, detections
+
+    def print_detections(self, detections):
+        print("\nDetections:")
+        for class_id in range(len(self.unified_language_model.detection_classes)):
+            print(
+                f"{self.unified_language_model.detection_classes[class_id]}: "
+                f"{len([d for d in detections if d[3] == class_id])}"
+            )
+
+    def display_debug_image(self, image, detections):
+        bounding_box_annotator = sv.BoxAnnotator()
+        annotated_image = bounding_box_annotator.annotate(
+            scene=image,
+            detections=detections,
+        )
+
+        labels = [
+            f"{self.DETECTION_CLASSES[class_id]} {confidence:0.2f}"
+            for _, _, confidence, class_id, _, _ in detections
+        ]
+
+        label_annotator = sv.LabelAnnotator()
+        annotated_image = label_annotator.annotate(
+            annotated_image,
+            detections,
+            labels,
+        )
+
+        height, width = image.shape[:2]
+        annotated_image = cv2.resize(annotated_image, (width // 2, height // 2))
+        cv2.imshow("Anonymizer Debug", annotated_image)
+        cv2.waitKey(1)
+
+
+def anonymize_with_unified_model(config_data, json_data, device):
+    rclpy.init()
+    try:
+        node = ImageAnonymizerNode(config_data, json_data, device)
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        rclpy.shutdown()
--- main.py
+++ main.py
@@ -22,28 +22,28 @@ from autoware_rosbag2_anonymizer.tools.validator import (
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="ROS2 bag anonymizer tool.")
+    parser = argparse.ArgumentParser(description="ROS2 image anonymizer tool.")
 
     parser.add_argument("config", type=str, help="Path to the config file")
     parser.add_argument(
         "--anonymize_with_unified_model",
         action="store_true",
-        help="Give a single ROS2 bag file as an input and anonymize bag file combining Grounding DINO, OpenCLIP and SAM.",
+        help="Run a ROS 2 node to anonymize images received from a subscription topic using Grounding DINO, OpenCLIP, and SAM.",
     )
     parser.add_argument(
         "--yolo_create_dataset",
         action="store_true",
-        help="Create initial dataset in YOLO format with combining Grounding DINO, OpenCLIP and SAM.",
+        help="Create initial dataset in YOLO format by combining Grounding DINO, OpenCLIP, and SAM.",
     )
     parser.add_argument(
         "--yolo_train",
         action="store_true",
-        help="Train YOLO with initial dataset",
+        help="Train YOLO with the created dataset.",
     )
     parser.add_argument(
         "--yolo_anonymize",
         action="store_true",
-        help="Give a single ROS2 bag file as an input and anonymize bag file with trained YOLO model.",
+        help="Run YOLO to anonymize images received from a subscription topic.",
     )
     parser.add_argument(
         "--validation",
@@ -63,7 +63,7 @@ def parse_arguments():
         ]
     ):
         parser.error(
-            "Please select one of --anonymize_with_unified_model, --yolo_create_dataset, --yolo_train, or --yolo_anonymize."
+            "Please select one of --anonymize_with_unified_model, --yolo_create_dataset, --yolo_train, --yolo_anonymize, or --validation."
         )
 
     return args
@@ -74,12 +74,15 @@ if __name__ == "__main__":
 
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
+    # Load configuration file
     with open(args.config, "r") as file:
         config_data = yaml.safe_load(file)
 
+    # Load detection classes or model-specific metadata
     with open("validation.json", "r") as json_file:
         json_data = json.load(json_file)
 
+    # Determine the selected action and execute the corresponding function
     if args.anonymize_with_unified_model:
         anonymize_with_unified_model(
             config_data=config_data,
